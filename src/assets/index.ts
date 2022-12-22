2ecc84b5fe58f76146b03eb479e98678e0b60baa
GREENFONTS <godwillonyewuchii@gmail.com>
2022-12-22 04:52:47 +0100

    fix: image prod issue

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,9 +2,9 @@ export { default as CloseMenu } from "./svg/CloseMenu.svg";
 export { default as MenuIcon } from "./svg/MenuIcon.svg";
 export { default as React } from "./svg/react.svg";
 
-export { default as Heed } from "./Images/heed.png";
-export { default as StreetRates } from "./Images/StreetRates.png";
-export { default as EasyVideo } from "./Images/EasyVideo.png";
-export { default as FinCash } from "./Images/FinCash.png";
-export { default as MetaNfts } from "./Images/MetaNfts.png";
-export { default as VotingApp } from "./Images/VotingApp.png";
+export { default as Heed } from "./img/heed.png";
+export { default as StreetRates } from "./img/StreetRates.png";
+export { default as EasyVideo } from "./img/EasyVideo.png";
+export { default as FinCash } from "./img/FinCash.png";
+export { default as MetaNfts } from "./img/MetaNfts.png";
+export { default as VotingApp } from "./img/VotingApp.png";
