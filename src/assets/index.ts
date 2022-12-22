df16db127d477d8923837c292ecc275d2426c6d7
GREENFONTS <godwillonyewuchii@gmail.com>
2022-12-22 04:54:55 +0100

    fix image import

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,7 @@ export { default as CloseMenu } from "./svg/CloseMenu.svg";
 export { default as MenuIcon } from "./svg/MenuIcon.svg";
 export { default as React } from "./svg/react.svg";
 
-export { default as Heed } from "./img/heed.png";
+export { default as Heed } from "./img/Heed.png";
 export { default as StreetRates } from "./img/StreetRates.png";
 export { default as EasyVideo } from "./img/EasyVideo.png";
 export { default as FinCash } from "./img/FinCash.png";
